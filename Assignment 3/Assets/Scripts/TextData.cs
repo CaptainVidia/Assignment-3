abbde97fc563e0429fc1c00a8f3dca73d773aff8
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2021-02-14 18:30:47 -0600

    Minor Changes

--- Assignment 3/Assets/Scripts/TextData.cs
+++ Assignment 3/Assets/Scripts/TextData.cs
@@ -1,5 +1,4 @@
-﻿/* Source: Example Code for Game Programming Design Patterns by  Owen Schaffer
- * George Tang
+﻿/* George Tang
  * Assignment 3
  * TextData.cs
  * using methods from ISubject to remove, register notify, and setting text
@@ -12,31 +11,21 @@ using UnityEngine.UI;
 
 namespace ObserverPatternText
 {
-    //Attach this class to an InputField GameObject in the scene
-    public class TextData : MonoBehaviour, ISubject
+    public class TextData : MonoBehaviour, IQuiz
     {
         private List<IObserver> observers = new List<IObserver>();
-       
         private string text;
-        string correctAnswerText  = "Tesla";
-
-        public bool correctAnswer;
-
-        //Attach the InputField GameObject where the user types in 
-        //the text to this in the inspector
         public GameObject inputField;
         private object anwser;
 
-        public void RegisterObserver(IObserver observer)
+        public void RecordObserver(IObserver observer)
         {
-            //Add observer to list of observers
+            
             observers.Add(observer);
-
-            //Updates data for newly added observer
             observer.UpdateData(text);
         }
 
-        public void RemoveObserver(IObserver observer)
+        public void DeleteObserver(IObserver observer)
         {
             if (observers.Contains(observer))
             {
@@ -49,16 +38,13 @@ namespace ObserverPatternText
         {
             foreach (IObserver observer in observers)
             {
-                //include data as parameters to UpdateData
+                
                 observer.UpdateData(text);
-                //observer.CheckAnswer(anwser);
                 Debug.Log("UpdateData was called from Notify Observers");
                 
             }
         }
-
-        //This method must be called by Unity, such as by calling 
-        //it when a Submit button is pressed
+        
         public void setText()
         {
             this.text = inputField.GetComponent<Text>().text;
