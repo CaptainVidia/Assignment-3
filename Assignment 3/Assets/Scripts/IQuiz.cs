f3a57aff5285152f7c928bf903146a8051bb4613
CaptainVidia <46515050+CaptainVidia@users.noreply.github.com>
2021-02-14 18:26:59 -0600

    Minor Changes

---
+++ Assignment 3/Assets/Scripts/IQuiz.cs
@@ -0,0 +1,21 @@
+﻿/* George Tang
+ * Assignment 3
+ * ISubject.cs
+ * ObserverPatternText creates a interface and method for Register Remove and Notify
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObserverPatternText
+{
+
+    public interface IQuiz
+    {
+        void RecordObserver(IObserver observer);
+        void DeleteObserver(IObserver observer);
+        void NotifyObservers();
+    }
+
+
+}
